1b964f259fb04995b9dd447a83b204f0293fe764
Tim Shelburne <shelburt02@gmail.com>
2021-01-31 13:01:35 -0500

    Added and tested type preset interface

--- src/type-presets.test.js
+++ src/type-presets.test.js
@@ -3,7 +3,7 @@ const sass = require('sass')
 
 describe('scss', function() {
 	it('supports defining and using typescales', function() {
-		expect(`
+		const scss = `
 			@use 'type-presets' as t with (
 				$typescales: (
 					1: 12px (md: 14px, xl: 16px),
@@ -13,8 +13,8 @@ describe('scss', function() {
 
 			p {
 				@include t.typescale(1);
-			};
-			`).toRender(`
+			}`
+		const css = `
 			@media screen and (min-width: 0) {
 				:global {
 					--typescale-1-font-size: 12px;
@@ -59,7 +59,105 @@ describe('scss', function() {
 			p {
 				font-size: var(--typescale-1-font-size);
 				line-height: var(--typescale-1-line-height);
-			}`)
+			}`
+		expect(scss).toRender(css)
+	})
+
+	it('supports defining and using type presets', function() {
+		const scss = `
+			@use 'type-presets' as t with (
+				$default-family: 'Arial, sans-serif',
+				$typescales: (
+					1: 12px (md: 14px, xl: 16px),
+					2: 14px (lg: 20px),
+				),
+				$presets: (
+					1: (scale: 1, spacing: .5),
+					2: (scale: 1, family: 'Comic Sans', transform: uppercase),
+					3: (scale: 2, family: 'Comic Sans', weight: 800),
+				)
+			);
+
+			.preset-1 {
+				@include t.type-preset(1);
+			}
+
+			.preset-2 {
+				@include t.type-preset(2);
+			}
+
+			.preset-3 {
+				@include t.type-preset(3);
+			}`
+		const css = `
+			@media screen and (min-width: 0) {
+				:global {
+					--typescale-1-font-size: 12px;
+					--typescale-1-line-height: 20px;
+					--typescale-2-font-size: 14px;
+					--typescale-2-line-height: 22px;
+				}
+			}
+			@media screen and (min-width: 600px) {
+				:global {
+					--typescale-1-font-size: 12px;
+					--typescale-1-line-height: 20px;
+					--typescale-2-font-size: 14px;
+					--typescale-2-line-height: 22px;
+				}
+			}
+			@media screen and (min-width: 900px) {
+				:global {
+					--typescale-1-font-size: 14px;
+					--typescale-1-line-height: 22px;
+					--typescale-2-font-size: 14px;
+					--typescale-2-line-height: 22px;
+				}
+			}
+			@media screen and (min-width: 1200px) {
+				:global {
+					--typescale-1-font-size: 14px;
+					--typescale-1-line-height: 22px;
+					--typescale-2-font-size: 20px;
+					--typescale-2-line-height: 28px;
+				}
+			}
+			@media screen and (min-width: 1800px) {
+				:global {
+					--typescale-1-font-size: 16px;
+					--typescale-1-line-height: 24px;
+					--typescale-2-font-size: 20px;
+					--typescale-2-line-height: 28px;
+				}
+			}
+
+			.preset-1 {
+				font-size: var(--typescale-1-font-size);
+				line-height: var(--typescale-1-line-height);
+				font-family: Arial, sans-serif;
+				font-weight: normal;
+				text-transform: none;
+				letter-spacing: 0.5;
+			}
+
+			.preset-2 {
+				font-size: var(--typescale-1-font-size);
+				line-height: var(--typescale-1-line-height);
+				font-family: Comic Sans;
+				font-weight: normal;
+				text-transform: uppercase;
+				letter-spacing: normal;
+			}
+
+			.preset-3 {
+				font-size: var(--typescale-2-font-size);
+				line-height: var(--typescale-2-line-height);
+				font-family: Comic Sans;
+				font-weight: 800;
+				text-transform: none;
+				letter-spacing: normal;
+			}`
+		expect(scss).toRender(css)
 	})
 })
 
